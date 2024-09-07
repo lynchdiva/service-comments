a83a1825ad8f6e069026af2d38021907c3773459
lynchdiva <attamaill@gmail.com>
2024-09-07 13:23:49 +0400

    feat: add option write comment without name

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,7 +1,36 @@
 'use strict';
 
+makeChoosingSingleCheckbox();
+
 const btn = document.querySelector('.comments-service__btn');
+const checkboxNoName = document.querySelector(
+  '.comments-service__checkbox[value="no"]'
+);
+const checkboxYesName = document.querySelector(
+  '.comments-service__checkbox[value="yes"]'
+);
+
 btn.addEventListener('click', addComment);
+
+checkboxNoName.addEventListener('change', () => {
+  const label = document.querySelector('[for="name"]');
+  const input = document.querySelector('#name');
+
+  label.classList.add('hidden');
+  input.classList.add('hidden');
+
+  input.classList.remove('required');
+});
+
+checkboxYesName.addEventListener('change', () => {
+  const label = document.querySelector('[for="name"]');
+  const input = document.querySelector('#name');
+
+  label.classList.remove('hidden');
+  input.classList.remove('hidden');
+
+  input.classList.add('required');
+});
 
 function addComment() {
   if (checkEmptyField()) {
@@ -12,22 +41,18 @@ function addComment() {
   const templateComment = document.querySelector(
     '.comments-service__comment-box'
   );
-  const newComment = templateComment.cloneNode(true);
-  const chat = document.querySelector('.comments-service__chat');
 
   if (templateComment.classList.contains('hidden')) {
-    addCommentTextTo(templateComment);
-    addNameTo(templateComment);
-    addAvatarTo(templateComment);
-
+    fillInfo(templateComment);
     templateComment.classList.remove('hidden');
 
     return;
   }
 
-  addCommentTextTo(newComment);
-  addNameTo(newComment);
-  addAvatarTo(newComment);
+  const newComment = templateComment.cloneNode(true);
+  const chat = document.querySelector('.comments-service__chat');
+
+  fillInfo(newComment);
 
   chat.append(newComment);
 }
@@ -50,16 +75,24 @@ function addWarningClass(elem) {
   elem.classList.add('warning');
 
   elem.addEventListener('focus', function handler() {
-    this.classList.remove('warning');
-    this.removeEventListener('focus', handler);
+    elem.classList.remove('warning');
+    elem.removeEventListener('focus', handler);
   });
 }
 
 function showTooltip() {
   const tooltip = document.querySelector('.comments-service__tooltip');
+
   tooltip.classList.add('visible');
 
   setTimeout(() => tooltip.classList.remove('visible'), 1500);
+}
+
+function fillInfo(elem) {
+  addCommentTextTo(elem);
+  addNameTo(elem);
+  addDateTo(elem);
+  addAvatarTo(elem);
 }
 
 function addCommentTextTo(elem) {
@@ -82,10 +115,16 @@ function getFilteredComment(comment) {
 
 function addNameTo(elem) {
   const nameInput = document.querySelector('#name');
-  const name = nameInput.value;
-  const reformedName = getReformedName(name);
 
-  elem.querySelector('.comments-service__nickname').textContent = reformedName;
+  let name = '';
+
+  if (nameInput.classList.contains('hidden')) {
+    name = 'Username';
+  } else {
+    name = getReformedName(nameInput.value);
+  }
+
+  elem.querySelector('.comments-service__nickname').textContent = name;
 
   nameInput.value = '';
 }
@@ -96,6 +135,47 @@ function getReformedName(name) {
     formattedName[0].toUpperCase() + formattedName.slice(1).toLowerCase();
 
   return formattedName;
+}
+
+function addDateTo(elem) {
+  const time = elem.querySelector('.comments-service__comment-time');
+
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth();
+  const day = date.getDate();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const monthNames = [
+    'января',
+    'февраля',
+    'марта',
+    'апреля',
+    'мая',
+    'июня',
+    'июля',
+    'августа',
+    'сентября',
+    'октября',
+    'ноября',
+    'декабря'
+  ];
+  const monthName = monthNames[month];
+
+  time.textContent = `${day} ${monthName} ${year} ${addZero(hours)}:${addZero(
+    minutes
+  )}`;
+
+  time.setAttribute(
+    'datetime',
+    `${year}-${addZero(month + 1)}-${addZero(day)}T${addZero(hours)}:${addZero(
+      minutes
+    )}`
+  );
+
+  function addZero(num) {
+    return String(num).length > 1 ? num : '0' + num;
+  }
 }
 
 function addAvatarTo(elem) {
@@ -109,7 +189,24 @@ function addAvatarTo(elem) {
 
   avatar.addEventListener('error', function handler() {
     console.log('Ошибка при загрузке изображения.');
-    this.src = defaultAvatarPath;
+    avatar.src = defaultAvatarPath;
     avatar.removeEventListener('focus', handler);
   });
+}
+
+function makeChoosingSingleCheckbox() {
+  const checkboxes = document.querySelectorAll('.comments-service__checkbox');
+
+  checkboxes.forEach(checkbox =>
+    checkbox.addEventListener('change', () => {
+      checkboxes.forEach(cb => {
+        if (cb !== checkbox) {
+          cb.checked = false;
+        }
+        if (cb === checkbox && !cb.checked) {
+          cb.checked = true;
+        }
+      });
+    })
+  );
 }
